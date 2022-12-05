b2061e4bb1035b4b9b740760b6e3e524b79677ff
Tomas Arcaria <tarcaria@gmail.com>
2022-12-05 07:35:21 -0300

    Aplicando vue cli

---
+++ proyecto-curso/src/components/PokeList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="">
+            <table class="p-10">
+                <tr class="bg-[#696969] text-[#fff]">
+                    <td class="h-10 w-20 text-center">Imagen</td>
+                    <td class="h-10 w-40 text-center">Nombre</td>
+                    <td class="h-10 w-20 text-center">Tipo</td>
+                    <td class="h-10 w-20 text-center">Número</td>
+                </tr>
+                <tr v-for="(poke, index) of generacion" 
+                    :key="index" 
+                    :class="[poke.bg,poke.color]"
+                    >
+                    <td class="flex items-center justify-center"><img :src="poke.imagen" width="32" height="32"></td>
+                    <td class="text-center">{{poke.nombre}}</td>
+                    <td class="text-center">{{poke.tipo}}</td>
+                    <td class="text-center">{{poke.numero}}</td>
+                </tr>
+            </table>
+        </div>
+</template>
+
+<script>
+export default {
+    
+    props: {
+        generacion: Array,
+    }
+
+}
+</script>
